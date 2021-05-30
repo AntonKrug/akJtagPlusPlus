4272f644b454998b1a15f6243854a3c56e714b2b
Anton Krug <anton.krug@gmail.com>
2021-05-30 15:22:04 +0100

    Cleaner demo example

--- Core/Src/jtag/jtag_c_connector.cpp
+++ Core/Src/jtag/jtag_c_connector.cpp
@@ -27,16 +27,19 @@ void jtag_setup() {
 
 void jtag_loop() {
 
-  jtag::tap::reset();
-      for (int number = 0; number < jtag::tap::tapStateSize; number++) {
-        jtag::tap::stateMove(static_cast<jtag::tap::state_e>(number));
-        jtag::bitbang::shiftTms({8, 0b11111111});
-        jtag::bitbang::shiftTmsRaw(32, number);
-      }
+  jtag::bitbang::resetTarget(32);
+  jtag::bitbang::resetTargetRelease(32);
+
+  jtag::tap::reset();  // Redundant, after target reset the tap would be in the reset anyway
+
+  for (int number = 0; number < jtag::tap::tapStateSize; number++) {
+    jtag::tap::stateMove(static_cast<jtag::tap::state_e>(number));
+    jtag::bitbang::shiftTms({8, 0b11111111});
+    jtag::bitbang::shiftTmsRaw(32, number);
+  }
 
   jtag::tap::stateMove(jtag::tap::state_e::RunTestIdle);
   jtag::tap::stateMove(jtag::tap::state_e::UpdateIr);
-
 }
 
 
