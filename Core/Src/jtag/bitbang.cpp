e511f3938cf79f53e6fe7501c0475109e3477b4d
Anton Krug <anton.krug@gmail.com>
2021-05-30 14:43:53 +0100

    Fixing typos

--- Core/Src/jtag/bitbang.cpp
+++ Core/Src/jtag/bitbang.cpp
@@ -19,6 +19,7 @@ namespace jtag {
     const uint8_t TMS = 3;
     const uint8_t TDI = 4;
     const uint8_t TDO = 5;
+    const uint8_t RST = 6;
 
     template<uint8_t WHAT_SIGNAL>
     __attribute__((optimize("-Ofast")))
@@ -35,7 +36,6 @@ namespace jtag {
         asm("nop");
         GPIOE->ODR = ((number & 1) << WHAT_SIGNAL) | (1 << TCK);
         ret = ret << 1 | ( (GPIOE->IDR >> TDI) & 0x1);
-        asm("nop");
         number = number >> 1;
       }
       return ret;
@@ -58,7 +58,7 @@ namespace jtag {
     // GPIOE->ODR => 0x40000000 + 0x00020000UL + 0x1000UL + 0x14 => 0x40021014
     template<uint8_t WHAT_SIGNAL>
     __attribute__((optimize("-Ofast")))
-    uint32_t shiftAsm(const uint32_t lenght, uint32_t write_value) {
+    uint32_t shiftAsm8(const uint32_t length, uint32_t write_value) {
       uint32_t addressWrite = GPIOE_BASE + 0x14; // ODR register of GPIO port E
       uint32_t addressRead  = GPIOE_BASE + 0x10; // IDR register of GPIO port E
 
@@ -69,33 +69,34 @@ namespace jtag {
 
       asm volatile (
         "cpsid if                                                  \n\t"  // Disable IRQ
+
         "repeatForEachBit%=:                                       \n\t"
 
         // Low part of the TCK
         "and.w %[shift_out],   %[write_value],    #1               \n\t"  // shift_out = write_value & 1
-        "lsls  %[shift_out],   %[shift_out],      %[write_shift]   \n\t"  // shift_out = shift_out << pin_shift
+        "lsls  %[shift_out],   %[shift_out],      %[write_shift]   \n\t"  // shift_out = shift_out << write_shift
         "str   %[shift_out],   [%[gpio_out_addr]]                  \n\t"  // GPIO = shift_out
 
         // On first cycle this is redundant, as it processed the shift_in from the previous iteration
-        "lsr   %[shift_in],    %[shift_in],       %[read_shift]    \n\t"  // shift_in = shift_in >> TDI
+        "lsr   %[shift_in],    %[shift_in],       %[read_shift]    \n\t"  // shift_in = shift_in >> (pin # of TDI)
         "and.w %[shift_in],    %[shift_in],       #1               \n\t"  // shift_in = shift_in & 1
         "lsl   %[ret_value],   #1                                  \n\t"  // ret = ret << 1
         "orr.w %[ret_value],   %[ret_value],      %[shift_in]      \n\t"  // ret = ret | shift_in
 
-        // Prepare things which are needed torward end of the loop, but can be done now
-        "orr.w %[shift_out],   %[shift_out],      %[clock_mask]    \n\t"  // shift_out = shift_out | TCK
+        // Prepare things that are needed toward the end of the loop, but can be done now
+        "orr.w %[shift_out],   %[shift_out],      %[clock_mask]    \n\t"  // shift_out = shift_out | (1 << TCK)
         "lsr   %[write_value], %[write_value],    #1               \n\t"  // write_value = write_value >> 1
         "adds  %[count],       #1                                  \n\t"  // count++
-        "cmp   %[count],       %[lenght]                           \n\t"  // if (count != lenght) then ....
+        "cmp   %[count],       %[length]                           \n\t"  // if (count != length) then ....
 
         // High part of the TCK + sample
         "str   %[shift_out],   [%[gpio_out_addr]]                  \n\t"  // GPIO = shift_out
         "nop                                                       \n\t"
         "nop                                                       \n\t"
         "ldr   %[shift_in],    [%[gpio_in_addr]]                   \n\t"  // shift_in = GPIO
-        "bne.n repeatForEachBit%=                                  \n\t"  // if (count != lenght) then  repeatForEachBit
+        "bne.n repeatForEachBit%=                                  \n\t"  // if (count != length) then  repeatForEachBit
 
-        "cpsie if                                                  \n\t"  // Enable IRQ
+        "cpsie if                                                  \n\t"  // Enable IRQ, the critical section finished
 
         // Process the shift_in as normally it's done in the next iteration of the loop
         "lsr   %[shift_in],    %[shift_in],       %[read_shift]    \n\t"  // shift_in = shift_in >> TDI
@@ -113,7 +114,7 @@ namespace jtag {
         // Inputs
         : [gpio_out_addr]   "r"(addressWrite),
           [gpio_in_addr]    "r"(addressRead),
-          [lenght]          "r"(lenght),
+          [length]          "r"(length),
           [write_value]     "r"(write_value),
           [write_shift]     "M"(WHAT_SIGNAL),
           [read_shift]      "M"(TDO),
@@ -128,8 +129,7 @@ namespace jtag {
 
 
     void shiftTms(jtag::tap::tmsMove move) {
-      shiftAsm<TMS>(move.amountOfBitsToShift, move.valueToShift);
-//      shift<TMS>(move.amountOfBitsToShift, move.valueToShift);
+      shiftAsm8<TMS>(move.amountOfBitsToShift, move.valueToShift);
     }
 
 
