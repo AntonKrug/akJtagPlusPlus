a1cdc63b14804d536ab359404629c2884903760e
Anton Krug <anton.krug@gmail.com>
2021-05-30 04:35:57 +0100

    Disabling and Enabling IRQ

--- Core/Src/jtag/bitbang.cpp
+++ Core/Src/jtag/bitbang.cpp
@@ -54,35 +54,38 @@ namespace jtag {
     template<uint8_t WHAT_SIGNAL>
     __attribute__((optimize("-Ofast")))
     void shiftAsm(const uint32_t lenght, uint32_t write_value) {
-      volatile uint32_t addressWrite = GPIOE_BASE + 0x14; // Is the ODR register
+      volatile uint32_t addressWrite = GPIOE_BASE + 0x14; // ODR register of GPIO port E
+      volatile uint32_t addressRead  = GPIOE_BASE + 0x10; // IDR register of GPIO port E
       uint32_t count        = 0;
       uint32_t read_value;
       uint32_t value_shifted;
       asm volatile (
-        //   "mov %[read_value],  10                             \n\t"
-        "repeatForEachBit%=:                                   \n\t"
+        //   "mov %[read_value],  10                               \n\t"
+        "cpsid if                                                  \n\t"  // Disable IRQ
+        "repeatForEachBit%=:                                       \n\t"
 
         // Low part of the TCK
-        "and %[value_shifted], %[write_value],   #1            \n\t"  // value_shifted = value_shifted & pin_mask
-        "lsl %[value_shifted], %[value_shifted], %[pin_shift]  \n\t"  // value_shifted = value_shifted << pin_shift
-        "str %[value_shifted], [%[gpio_out_addr]]              \n\t"  // GPIO = value_shifted
-        "nop                                                   \n\t"
-        "nop                                                   \n\t"
-        "nop                                                   \n\t"
-        "nop                                                   \n\t"
-        "nop                                                   \n\t"
-        "nop                                                   \n\t"
-        "nop                                                   \n\t"
-        "nop                                                   \n\t"
+        "and.w %[value_shifted], %[write_value],    #1             \n\t"  // value_shifted = write_value   & 1
+        "lsl   %[value_shifted], %[value_shifted],  %[pin_shift]   \n\t"  // value_shifted = value_shifted << pin_shift
+        "str   %[value_shifted], [%[gpio_out_addr]]                \n\t"  // GPIO = value_shifted
+        "nop                                                       \n\t"
+        "nop                                                       \n\t"
+        "nop                                                       \n\t"
+        "nop                                                       \n\t"
+        "nop                                                       \n\t"
+        "nop                                                       \n\t"
+        "nop                                                       \n\t"
+        "nop                                                       \n\t"
 
         // High part of the TCK
-        "orr %[value_shifted], %[value_shifted], %[clock_mask] \n\t"  // value_shifted = value_shifted | TCK
-        "str %[value_shifted], [%[gpio_out_addr]]              \n\t"  // GPIO = value_shifted
-        "nop                                                   \n\t"
-        "lsr %[write_value],   %[write_value],   #1            \n\t"  // write_value = write_value >> 1
-        "add %[count],         %[count],         #1            \n\t"  // count++
-        "cmp %[count],         %[lenght]                       \n\t"  // if (count != lenght) then
-        "bne repeatForEachBit%=                                \n\t"  //   repeatForEachBit
+        "orr.w %[value_shifted], %[value_shifted],  %[clock_mask]  \n\t"  // value_shifted = value_shifted | TCK
+        "str   %[value_shifted], [%[gpio_out_addr]]                \n\t"  // GPIO = value_shifted
+        "nop                                                       \n\t"
+        "lsr   %[write_value],   %[write_value],    #1             \n\t"  // write_value = write_value >> 1
+        "add.w %[count],         %[count],          #1             \n\t"  // count++
+        "cmp   %[count],         %[lenght]                         \n\t"  // if (count != lenght) then
+        "bne   repeatForEachBit%=                                  \n\t"  //   repeatForEachBit
+        "cpsie if                                                  \n\t"  // Enable IRQ
 
         // Outputs
         : [read_value]      "=r"(read_value),
@@ -104,6 +107,7 @@ namespace jtag {
 
     void shiftTms(jtag::tap::tmsMove move) {
       shiftAsm<TMS>(move.amountOfBitsToShift, move.valueToShift);
+      shift<TMS>(move.amountOfBitsToShift, move.valueToShift);
     }
 
 
