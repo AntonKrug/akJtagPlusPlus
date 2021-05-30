f44ba32a8e51c88dff41423f786b46a449a386a2
Anton Krug <anton.krug@gmail.com>
2021-05-30 19:43:51 +0100

    Global define to enable/disable tap state machine telemetry

---
+++ Core/Src/jtag/jtag_global.h
@@ -0,0 +1,16 @@
+/*
+ * jtag_global.h
+ *
+ *  Created on: May 30, 2021
+ *      Author: anton.krug@gmail.com
+ *     License: GPLv2
+ */
+
+#ifndef SRC_JTAG_JTAG_GLOBAL_H_
+#define SRC_JTAG_JTAG_GLOBAL_H_
+
+// Uncomment the telemetry define to disable the TAP state machine telemetry
+#define JTAG_TAP_TELEMETRY
+
+
+#endif /* SRC_JTAG_JTAG_GLOBAL_H_ */
