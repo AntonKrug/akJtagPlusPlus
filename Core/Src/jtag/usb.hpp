f5a37dcd6852f41f2cc8e05b61fe1fa6fac2c178
Anton Krug <anton.krug@gmail.com>
2021-06-10 21:50:32 +0100

    Reorganizing API + comments

--- Core/Src/jtag/usb.hpp
+++ Core/Src/jtag/usb.hpp
@@ -13,26 +13,45 @@ namespace jtag {
 
   namespace usb {
 
+
+
+
     enum class api_e:uint32_t {
-      ping,
-      reset,
+      end_processing, // do not process any other command from the buffer
+
+      ping,           // respond back what version this FW is
+
+      reset,          // trst or srst
       setLed,
-      setSpeed,
-      getSpeed,
-      stateMove,     // Specify endState after commands (don't change TAP SM)
-      pathMove,      // move current state to the endstate (change TAP SM)
+      setSpeed,       // set TCK speed
+      getSpeed,       // get TCK speed
+
+      stateMove,      // Specify endState after commands (don't change TAP SM)
+      pathMove,       // move current state to the endstate (change TAP SM)
       runTest,
-      setIrOpcodeLen,
-      setDrOpcodeLen,
-      scanIrRw2,
-      scanIr2,
-      scanDrRw2,
-      scanDr2,
-      scanIrRw1,
-      scanIr1,
-      scanDrRw1,
-      scanDr1,
-    }
+
+      setIrOpcodeLen, // max 32bits
+      setDrOpcodeLen, // max 32bits
+
+      scanIrRw1,      // Read and write IR, 1 argument  (data) => (len and endState are inferred)
+      scanIr1,        // Write IR,          1 argument  (data) => (len and endState are inferred)
+      scanDrRw1,      // Read and write DR, 1 argument  (data) => (len and endState are inferred)
+      scanDr1,        // Write DR,          1 argument  (data) => (len and endState are inferred)
+
+      scanIrRw2,      // Read and write IR, 2 arguments (endState, data) => (len is inferred)
+      scanIr2,        // Write IR,          2 arguments (endState, data) => (len is inferred)
+      scanDrRw2,      // Read and write DR, 2 arguments (endState, data) => (len is inferred)
+      scanDr2,        // Write DR,          2 arguments (endState, data) => (len is inferred)
+
+      // 3 argument scans shouldn't be needed as changing the Opcode len on each scan is unlikely
+      // and even if it would happen, the existing commands can achieve the same with just 1 word overhead
+
+      last_enum
+    };
+
+
+
+
   }
 }
 
