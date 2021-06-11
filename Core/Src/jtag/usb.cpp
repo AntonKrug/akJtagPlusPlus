20274eac2cfdd8dbc88507b64aa6b10be9b629d3
Anton Krug <anton.krug@gmail.com>
2021-06-11 01:15:23 +0100

    First increment done by parent

--- Core/Src/jtag/usb.cpp
+++ Core/Src/jtag/usb.cpp
@@ -17,16 +17,18 @@ namespace jtag {
 
 
     void skip(uint32_t **reqHandle, uint32_t **resHandle) {
-      (*reqHandle)++;
     }
 
 
     // Respond to ping with own FW version
     void ping(uint32_t **reqHandle, uint32_t **resHandle) {
-      (*reqHandle)++;               // Just read the command from queue, point to the next command
-
       **resHandle=JTAG_FW_VERSION;  // Populate response
       (*resHandle)++;               // Increment response buffer
+    }
+
+
+    void reset(uint32_t **reqHandle, uint32_t **resHandle) {
+
     }
 
 
@@ -42,6 +44,7 @@ namespace jtag {
       // and do while checks later (for the next command in queue)
       // should save one check
       uint32_t commandId = *req;
+      req++;
 
       do {
         if (commandId >= api_e_size) break; // Command outside the API
@@ -50,6 +53,7 @@ namespace jtag {
         handlers[commandId](&req, &res);
 
         commandId = *req;
+        req++;
       } while (commandId);
 
     }
