2452d9f18c857f5af254b1a1dcabf4834cac4171
Anton Krug <anton.krug@gmail.com>
2021-06-11 02:17:22 +0100

    CamelCase renaming

--- Core/Src/jtag/bitbang.cpp
+++ Core/Src/jtag/bitbang.cpp
@@ -42,15 +42,15 @@ namespace jtag {
     // GPIOE->ODR => 0x40000000 + 0x00020000UL + 0x1000UL + 0x14 => 0x40021014
     template<uint8_t WHAT_SIGNAL, uint8_t nTRSTvalue>
     __attribute__((optimize("-Ofast")))
-    uint32_t shiftAsmUltraSpeed(const uint32_t length, uint32_t write_value) {
+    uint32_t shiftAsmUltraSpeed(const uint32_t length, uint32_t writeValue) {
       // This has 9.363MHz TCK at 50% duty cycle (removing the NOPs below can make it slightly faster and with duty 48% or below)
       uint32_t addressWrite = GPIOE_BASE + 0x14; // ODR register of GPIO port E
       uint32_t addressRead  = GPIOE_BASE + 0x10; // IDR register of GPIO port E
 
-      uint32_t count     = 0;  // Counting how many bits are processed. Starting from 1 up to 'length' (inclusive) value. Set here to 0, but the code will increment it to 1 before the first check
-      uint32_t out_value = 0;  // Internal register to write values into the GPIO (driven by write_value, WHAT_SIGNAL, PIN_E_TCK and nTRSTvalue)
-      uint32_t in_value  = 0;  // Internal register to read raw values from GPIO and then masked/shifted correctly into the ret_value
-      uint32_t ret_value = 0;  // Output variable returning content from the TDI pin (driven from the in_value)
+      uint32_t count    = 0;  // Counting how many bits are processed. Starting from 1 up to 'length' (inclusive) value. Set here to 0, but the code will increment it to 1 before the first check
+      uint32_t outValue = 0;  // Internal register to write values into the GPIO (driven by writeValue, WHAT_SIGNAL, PIN_E_TCK and nTRSTvalue)
+      uint32_t inValue  = 0;  // Internal register to read raw values from GPIO and then masked/shifted correctly into the retValue
+      uint32_t retValue = 0;  // Output variable returning content from the TDI pin (driven from the inValue)
 
       asm volatile (
         "cpsid if                                                  \n\t"  // Disable IRQ temporary for critical moment
@@ -58,64 +58,64 @@ namespace jtag {
         "repeatForEachBit%=:                                       \n\t"
 
         // Low part of the TCK
-        "and.w %[out_value],   %[write_value],    #1               \n\t"  // out_value = write_value & 1
-        "lsls  %[out_value],   %[out_value],      %[write_shift]   \n\t"  // out_value = out_value << write_shift
-        "orr.w %[out_value],   %[out_value],      %[reset_value]   \n\t"  // out_value = out_value | (nRSTvlaue << nRST)
-        "str   %[out_value],   [%[gpio_out_addr]]                  \n\t"  // GPIO = out_value
+        "and.w %[outValue],    %[writeValue],     #1               \n\t"  // outValue = writeValue & 1
+        "lsls  %[outValue],    %[outValue],       %[writeShift]    \n\t"  // outValue = outValue << writeShift
+        "orr.w %[outValue],    %[outValue],       %[resetValue]    \n\t"  // outValue = outValue | (nRSTvlaue << nRST)
+        "str   %[outValue],    [%[gpioOutAddr]]                    \n\t"  // GPIO = outValue
 
-        // On first cycle this is redundant, as it processed the in_value from the previous iteration
+        // On first cycle this is redundant, as it processed the inValue from the previous iteration
         // The first iteration is safe to do extraneously as it's just doing zeros
-        "and.w %[in_value],    %[in_value],       %[read_mask]     \n\t"  // in_value = in_value & ( 1 << TDI)
-        "lsl   %[in_value],    %[in_value],       %[read_shift]    \n\t"  // in_value = in_value >> (pin # of TDI)
-        "lsr   %[ret_value],   #1                                  \n\t"  // ret_value = ret_value >> 1
-        "orr.w %[ret_value],   %[ret_value],      %[in_value]      \n\t"  // ret_value = ret_value | in_value
+        "and.w %[inValue],     %[inValue],        %[readMask]      \n\t"  // inValue = inValue & ( 1 << TDI)
+        "lsl   %[inValue],     %[inValue],        %[readShift]     \n\t"  // inValue = inValue >> (pin # of TDI)
+        "lsr   %[retValue],    #1                                  \n\t"  // retValue = retValue >> 1
+        "orr.w %[retValue],    %[retValue],       %[inValue]       \n\t"  // retValue = retValue | inValue
 
         // Prepare things that are needed toward the end of the loop, but can be done now
-        "orr.w %[out_value],   %[out_value],      %[clock_mask]    \n\t"  // out_value = out_value | (1 << TCK) - setting TCK high
-        "lsr   %[write_value], %[write_value],    #1               \n\t"  // write_value = write_value >> 1
+        "orr.w %[outValue],    %[outValue],       %[clock_mask]    \n\t"  // outValue = outValue | (1 << TCK) - setting TCK high
+        "lsr   %[writeValue],  %[writeValue],     #1               \n\t"  // writeValue = writeValue >> 1
         "adds  %[count],       #1                                  \n\t"  // count++
         "cmp   %[count],       %[length]                           \n\t"  // if (count != length) then ....
 
         // High part of the TCK + sample
-        "str   %[out_value],   [%[gpio_out_addr]]                  \n\t"  // GPIO = out_value
+        "str   %[outValue],    [%[gpioOutAddr]]                    \n\t"  // GPIO = outValue
         "nop                                                       \n\t"
-        "ldr   %[in_value],    [%[gpio_in_addr]]                   \n\t"  // in_value = GPIO
+        "ldr   %[inValue],     [%[gpioInAddr]]                     \n\t"  // inValue = GPIO
         "bne.n repeatForEachBit%=                                  \n\t"  // if (count != length) then  repeatForEachBit
 
         "cpsie if                                                  \n\t"  // Enable IRQ, the critical section finished
 
-        // Process the in_value as normally it's done in the next iteration of the loop
-        "and.w %[in_value],    %[in_value],       %[read_mask]     \n\t"  // in_value = in_value & ( 1 << TDI)
-        "lsl   %[in_value],    %[in_value],       %[read_shift]    \n\t"  // in_value = in_value >> (pin # of TDI)
-        "lsr   %[ret_value],   #1                                  \n\t"  // ret_value = ret_value >> 1
-        "orr.w %[ret_value],   %[ret_value],      %[in_value]      \n\t"  // ret_value = ret_value | in_value
+        // Process the inValue as normally it's done in the next iteration of the loop
+        "and.w %[inValue],     %[inValue],        %[readMask]      \n\t"  // inValue = inValue & ( 1 << TDI)
+        "lsl   %[inValue],     %[inValue],        %[readShift]     \n\t"  // inValue = inValue >> (pin # of TDI)
+        "lsr   %[retValue],    #1                                  \n\t"  // retValue = retValue >> 1
+        "orr.w %[retValue],    %[retValue],       %[inValue]       \n\t"  // retValue = retValue | inValue
 
 
         // Outputs
-        : [ret_value]       "+r"(ret_value),
+        : [retValue]        "+r"(retValue),
           [count]           "+r"(count),
-          [out_value]       "+r"(out_value),
-          [in_value]        "+r"(in_value)
+          [outValue]        "+r"(outValue),
+          [inValue]         "+r"(inValue)
 
         // Inputs
-        : [gpio_out_addr]   "r"(addressWrite),
-          [gpio_in_addr]    "r"(addressRead),
+        : [gpioOutAddr]     "r"(addressWrite),
+          [gpioInAddr]      "r"(addressRead),
           [length]          "r"(length),
-          [write_value]     "r"(write_value),
-          [write_shift]     "M"(WHAT_SIGNAL),
-          [read_shift]      "M"(31-PIN_E_TDO),
-          [read_mask]       "I"(powerOfTwo<PIN_E_TDO>()),
+          [writeValue]      "r"(writeValue),
+          [writeShift]      "M"(WHAT_SIGNAL),
+          [readShift]       "M"(31-PIN_E_TDO),
+          [readMask]        "I"(powerOfTwo<PIN_E_TDO>()),
           [clock_mask]      "I"(powerOfTwo<PIN_E_TCK>()),
-          [reset_value]     "I"(nTRSTvalue << PIN_E_nTRST)
+          [resetValue]      "I"(nTRSTvalue << PIN_E_nTRST)
 
         // Clobbers
         : "memory"
       );
 
       // Shift the rest of bits as they were pushed from opposite direction
-      ret_value = ret_value >> (32 - length);
+      retValue = retValue >> (32 - length);
 
-      return ret_value;
+      return retValue;
     }
 
 
@@ -126,16 +126,16 @@ namespace jtag {
     }
 
 
-    void shiftTmsRaw(uint32_t length, uint32_t write_value) {
+    void shiftTmsRaw(uint32_t length, uint32_t writeValue) {
       JTAG_SHIFT_TIMMING_START();
-      shiftAsmUltraSpeed<PIN_E_TMS, 1>(length, write_value);
+      shiftAsmUltraSpeed<PIN_E_TMS, 1>(length, writeValue);
       JTAG_SHIFT_TIMMING_END();
     }
 
 
-    uint32_t shiftTdi(uint32_t length, uint32_t write_value) {
+    uint32_t shiftTdi(uint32_t length, uint32_t writeValue) {
       JTAG_SHIFT_TIMMING_START();
-      return shiftAsmUltraSpeed<PIN_E_TDI, 1>(length, write_value);
+      return shiftAsmUltraSpeed<PIN_E_TDI, 1>(length, writeValue);
       JTAG_SHIFT_TIMMING_END();
     }
 
