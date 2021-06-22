f09d21ce8ad89eb9f92ea59ed9940b892e39dab8
Anton Krug <anton.krug@gmail.com>
2021-06-22 22:38:43 +0100

    Correct Report Description and 'echo' test

--- USB_DEVICE/App/usbd_custom_hid_if.c
+++ USB_DEVICE/App/usbd_custom_hid_if.c
@@ -32,7 +32,7 @@
 
 /* USER CODE BEGIN PV */
 /* Private variables ---------------------------------------------------------*/
-
+uint8_t buffer[USBD_CUSTOMHID_OUTREPORT_BUF_SIZE];
 /* USER CODE END PV */
 
 /** @addtogroup STM32_USB_OTG_DEVICE_LIBRARY
@@ -92,8 +92,21 @@
 __ALIGN_BEGIN static uint8_t CUSTOM_HID_ReportDesc_HS[USBD_CUSTOM_HID_REPORT_DESC_SIZE] __ALIGN_END =
 {
   /* USER CODE BEGIN 1 */
-  0x00,
-  /* USER CODE END 1 */
+   0x06, 0x00, 0xff, // Usage page (undefined)
+   0x09, 0x01,       // usage (undefined)
+   0xa1, 0x01,       // collection (application)
+   0x15, 0x00,       // logical minimim (0)
+   0x26, 0xff, 0x00, // logical maximim (255)
+   0x75, 0x08,       // report size (8)
+   0x95, 0x40,       // report count (64)
+   0x09, 0x01,       // usage (undefined)
+   0x81, 0x02,       // input (data, var, abs)
+   0x95, 0x40,       // report count (64)
+   0x09, 0x01,       // usage (undefined)
+   0x91, 0x02,       // output (data, var, abs)
+   0x95, 0x01,       // report count (1)
+   0x09, 0x01,       // usage (undefined)
+   0xb1, 0x02,       // feature (data, var, abs)  /* USER CODE END 1 */
    0xC0    /*     END_COLLECTION             */
 };
 /* USER CODE BEGIN PRIVATE_VARIABLES */
@@ -125,7 +138,7 @@ extern USBD_HandleTypeDef hUsbDeviceHS;
 
 static int8_t CUSTOM_HID_Init_HS(void);
 static int8_t CUSTOM_HID_DeInit_HS(void);
-static int8_t CUSTOM_HID_OutEvent_HS(uint8_t event_idx, uint8_t state);
+static int8_t CUSTOM_HID_OutEvent_HS(uint8_t* data);
 
 /**
   * @}
@@ -174,11 +187,13 @@ static int8_t CUSTOM_HID_DeInit_HS(void)
   * @param  state: Event state
   * @retval USBD_OK if all operations are OK else USBD_FAIL
   */
-static int8_t CUSTOM_HID_OutEvent_HS(uint8_t event_idx, uint8_t state)
+static int8_t CUSTOM_HID_OutEvent_HS(uint8_t* data)
 {
   /* USER CODE BEGIN 10 */
-  UNUSED(event_idx);
-  UNUSED(state);
+//  UNUSED(event_idx);
+//  UN  USED(state);
+  memcpy(buffer, data, 0x40);
+  USBD_CUSTOM_HID_SendReport(&hUsbDeviceHS, buffer, 0x40);
 
     /* Start next USB packet transfer once data processing is completed */
   USBD_CUSTOM_HID_ReceivePacket(&hUsbDeviceHS);
